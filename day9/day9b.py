c0f4939fdb27dd5bfa78985a7c527655bb441c17
Louis Clergue <louisclergue@gmail.com>
2019-12-09 22:00:42 +0100

    better args unpack

--- day9/day9b.py
+++ day9/day9b.py
@@ -1,6 +1,10 @@
 from collections import defaultdict
 from functools import partial
 
+def unpack(dic, start, size):
+    if size == 1: return dic[start]
+    return [dic[i] for i in range(start, start+size)]
+
 def opcode(code):
     op = code % 100
     code = code // 100
@@ -31,76 +35,60 @@ def program(memory):
         elif mode == 2: return mem[addr+off]
         else: raise Exception("wrong read" + [mem,off,mode,addr])
 
-    def unpack(arr, start, size):
-        return arr[start,start+size]
+    
+    args = partial(unpack, memory)
 
     while True:
         (op, [im1, im2, im3]) = opcode(memory[i])
         if op == 99:
             break
         elif op == 1: # ADD a b s
-            a = memory[i+1]
-            b = memory[i+2]
-            d = memory[i+3]
-            memory[write(d, im3)] = mem(a, im1) + mem(b, im2)
+            a, b, s = args(i+1, 3)
+            memory[write(s, im3)] = mem(a, im1) + mem(b, im2)
             i += 4
         elif op == 2: # MUL a b s
-            a = memory[i+1]
-            b = memory[i+2]
-            d = memory[i+3]
-            memory[write(d, im3)] = mem(a, im1) * mem(b, im2)
+            a, b, s = args(i+1, 3)
+            memory[write(s, im3)] = mem(a, im1) * mem(b, im2)
             i += 4
         elif op == 3: # IN s
-            a = memory[i+1]
+            a = args(i+1, 1)
             # input
             print("INPUT 2")
             memory[write(a, im1)] = 1
             i += 2
         elif op == 4: # OUT s
-            a = memory[i+1]
+            a = args(i+1, 1)
             res = mem(a, im1)
             print(res)
             i += 2
         elif op == 5: # NZ a j
-            a = memory[i+1]
-            b = memory[i+2]
+            a, j = args(i+1, 2)
             v = mem(a, im1)
             if v != 0:
-                i = mem(b, im2)
+                i = mem(j, im2)
             else:
                 i += 3
         elif op == 6: # Z a j
-            a = memory[i+1]
-            b = memory[i+2]
+            a, j = args(i+1, 2)
             v = mem(a, im1)
             if v == 0:
-                i = mem(b, im2)
+                i = mem(j, im2)
             else:
                 i += 3
         elif op == 7: # LT a b s
-            a = memory[i+1]
-            b = memory[i+2]
-            c = memory[i+3]
+            a, b, s = args(i+1, 3)
             v1 = mem(a, im1)
             v2 = mem(b, im2)
-            if v1 < v2:
-                memory[write(c, im3)] = 1
-            else:
-                memory[write(c,im3)] = 0
+            memory[write(s, im3)] = 1 if v1 < v2 else 0
             i += 4
         elif op == 8: # EQ a b s
-            a = memory[i+1]
-            b = memory[i+2]
-            c = memory[i+3]
+            a, b, s = args(i+1, 3)
             v1 = mem(a, im1)
             v2 = mem(b, im2)
-            if v1 == v2:
-                memory[write(c, im3)] = 1
-            else:
-                memory[write(c, im3)] = 0
+            memory[write(s, im3)] = 1 if v1 == v2 else 0
             i += 4
         elif op == 9: # OFF a
-            a = memory[i+1]
+            a = args(i+1, 1)
             mem_offset += mem(a, im1)
             i += 2
         else:

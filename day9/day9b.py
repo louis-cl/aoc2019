3826d2c7bdd13c12ec36bae3f32c9aef743f3828
Louis Clergue <louisclergue@gmail.com>
2019-12-09 22:24:07 +0100

    i is ptr, don't use partial for args

--- day9/day9b.py
+++ day9/day9b.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-from functools import partial
 
 def unpack(dic, start, size):
     if size == 1: return dic[start]
@@ -29,68 +28,67 @@ def writeWithMode(mem, addr, value, off=0, mode=0):
     else: raise Exception("wrong write" + [addr, value, off, mode])
 
 def program(memory):
-    i = 0
+    ptr = 0
     mem_offset = 0
-    args = partial(unpack, memory)
 
     while True:
-        (op, [im1, im2, im3]) = opcode(memory[i])
-        nargs = partial(args, i+1)
+        (op, [im1, im2, im3]) = opcode(memory[ptr])
+        args = lambda n: unpack(memory, ptr+1, n)
         read = lambda addr,m: readWithMode(memory, addr, mem_offset, m)
         write = lambda addr,m,v: writeWithMode(memory, addr, v, mem_offset, m)
         if op == 99:
             break
         elif op == 1: # ADD a b s
-            a, b, s = nargs(3)
+            a, b, s = args(3)
             val = read(a, im1) + read(b, im2)
             write(s, im3, val)
-            i += 4
+            ptr += 4
         elif op == 2: # MUL a b s
-            a, b, s = nargs(3)
+            a, b, s = args(3)
             val = read(a, im1) * read(b, im2)
             write(s, im3, val)
-            i += 4
+            ptr += 4
         elif op == 3: # IN s
-            a = nargs(1)
+            a = args(1)
             # input
             print("INPUT 2")
             write(a, im1, 1)
-            i += 2
+            ptr += 2
         elif op == 4: # OUT s
-            a = nargs(1)
+            a = args(1)
             res = read(a, im1)
             print(res)
-            i += 2
+            ptr += 2
         elif op == 5: # NZ a j
-            a, j = nargs(2)
+            a, j = args(2)
             v = read(a, im1)
             if v != 0:
-                i = read(j, im2)
+                ptr = read(j, im2)
             else:
-                i += 3
+                ptr += 3
         elif op == 6: # Z a j
-            a, j = nargs(2)
+            a, j = args(2)
             v = read(a, im1)
             if v == 0:
-                i = read(j, im2)
+                ptr = read(j, im2)
             else:
-                i += 3
+                ptr += 3
         elif op == 7: # LT a b s
-            a, b, s = nargs(3)
+            a, b, s = args(3)
             v1 = read(a, im1)
             v2 = read(b, im2)
             write(s, im3, 1 if v1 < v2 else 0)
-            i += 4
+            ptr += 4
         elif op == 8: # EQ a b s
-            a, b, s = nargs(3)
+            a, b, s = args(3)
             v1 = read(a, im1)
             v2 = read(b, im2)
             write(s, im3, 1 if v1 == v2 else 0)
-            i += 4
+            ptr += 4
         elif op == 9: # OFF a
-            a = nargs(1)
+            a = args(1)
             mem_offset += read(a, im1)
-            i += 2
+            ptr += 2
         else:
             print("something went wrong ", op)
             break;

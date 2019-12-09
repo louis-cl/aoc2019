d2baf7e6b3e7060fcb97b7a40cdb40493bef7281
Louis Clergue <louisclergue@gmail.com>
2019-12-09 22:04:52 +0100

    even less args for args jeje

--- day9/day9b.py
+++ day9/day9b.py
@@ -40,55 +40,56 @@ def program(memory):
 
     while True:
         (op, [im1, im2, im3]) = opcode(memory[i])
+        nargs = partial(args, i+1)
         if op == 99:
             break
         elif op == 1: # ADD a b s
-            a, b, s = args(i+1, 3)
+            a, b, s = nargs(3)
             memory[write(s, im3)] = mem(a, im1) + mem(b, im2)
             i += 4
         elif op == 2: # MUL a b s
-            a, b, s = args(i+1, 3)
+            a, b, s = nargs(3)
             memory[write(s, im3)] = mem(a, im1) * mem(b, im2)
             i += 4
         elif op == 3: # IN s
-            a = args(i+1, 1)
+            a = nargs(1)
             # input
             print("INPUT 2")
             memory[write(a, im1)] = 1
             i += 2
         elif op == 4: # OUT s
-            a = args(i+1, 1)
+            a = nargs(1)
             res = mem(a, im1)
             print(res)
             i += 2
         elif op == 5: # NZ a j
-            a, j = args(i+1, 2)
+            a, j = nargs(2)
             v = mem(a, im1)
             if v != 0:
                 i = mem(j, im2)
             else:
                 i += 3
         elif op == 6: # Z a j
-            a, j = args(i+1, 2)
+            a, j = nargs(2)
             v = mem(a, im1)
             if v == 0:
                 i = mem(j, im2)
             else:
                 i += 3
         elif op == 7: # LT a b s
-            a, b, s = args(i+1, 3)
+            a, b, s = nargs(3)
             v1 = mem(a, im1)
             v2 = mem(b, im2)
             memory[write(s, im3)] = 1 if v1 < v2 else 0
             i += 4
         elif op == 8: # EQ a b s
-            a, b, s = args(i+1, 3)
+            a, b, s = nargs(3)
             v1 = mem(a, im1)
             v2 = mem(b, im2)
             memory[write(s, im3)] = 1 if v1 == v2 else 0
             i += 4
         elif op == 9: # OFF a
-            a = args(i+1, 1)
+            a = nargs(1)
             mem_offset += mem(a, im1)
             i += 2
         else:

dfbf68a69ee3f30bc723d311a26448bc13962997
Louis Clergue <louisclergue@gmail.com>
2019-12-13 06:39:07 +0100

    update utils

--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
-def random():
-    return "HI"
+from math import gcd 
+from functools import reduce
+import re
+
+def lcm(denominators):
+    return reduce(lambda a,b: a*b // gcd(a,b), denominators)
+
+def integers(s):
+    return re.findall(r'-?\d+', s.strip())

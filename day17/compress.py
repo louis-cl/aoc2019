fa6a9319089b1530b12f7cf6c974735281b62163
Louis Clergue <louisclergue@gmail.com>
2019-12-17 21:57:32 +0100

    generalize regexp group finding

--- day17/compress.py
+++ day17/compress.py
@@ -1,9 +1,24 @@
 import re
 
-def regexp(st):
-    result = re.match(r'^(.{1,21})\1*(.{1,21})(?:\1|\2)*(.{1,21})(?:\1|\2|\3)*$', st)
+def from_regex(st, regex):
+    result = re.match(regex, st)
     methods = [result.group(i) for i in range(1,4)]
     routine = st
     for m,k in zip(methods,"ABC"):
         routine = routine.replace(m, k)
     return routine, methods
+
+def regexp(st):
+    return from_regex(st, r'^(.{1,21})\1*(.{1,21})(?:\1|\2)*(.{1,21})(?:\1|\2|\3)*$')
+    
+def iterative(st, methods=3, max_method_size=20):
+    patt = [r'^']
+    for i in range(1, methods+1):
+        patt.append(r'(.{{1,{}}})'.format(max_method_size+1))
+        if i == 1:
+            patt.append(r'\1*')
+        else:
+            middle = r'|'.join(map(lambda x: r'\{}'.format(x), range(1,i+1)))
+            patt.append(r'(?:{})*'.format(middle))
+    patt.append(r'$')
+    return from_regex(st, r''.join(patt))

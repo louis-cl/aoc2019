d3c2ea7937a81f06dbfbfe6e377d753b7656dda2
Louis Clergue <louisclergue@gmail.com>
2019-12-22 00:44:41 +0100

    add draw screen version to utils

--- utils.py
+++ utils.py
@@ -9,19 +9,30 @@ def integers(s):
     return re.findall(r'-?\d+', s.strip())
 
 # tiles is a dict complex -> char
-# top left is (0,0) -> (x,y)
-def draw(tiles, tileset=None):
+# top left is (0,0) -> x is row, y is column
+# screen means         x is column, y is row
+def draw(tiles, tileset=None, screen=False):
     max_y = int(max(z.imag for z in tiles.keys()))
     max_x = int(max(z.real for z in tiles.keys()))
 
     def tile(t):
         return tileset.get(t, t) if tileset else t
-
-    for i in range(max_x+1):
-        for j in range(max_y+1):
-            z = complex(i,j)
-            if z in tiles:
-                print(tile(tiles[z]), end='')
-            else:
-                print(' ', end='')
-        print()
+        
+    if screen:
+        for y in range(max_y+1):
+            for x in range(max_x+1):
+                z = complex(x,y)
+                if z in tiles:
+                    print(tile(tiles[z]), end='')
+                else:
+                    print(' ', end='')
+            print()
+    else:
+        for x in range(max_x+1):
+            for y in range(max_y+1):
+                z = complex(x,y)
+                if z in tiles:
+                    print(tile(tiles[z]), end='')
+                else:
+                    print(' ', end='')
+            print()
